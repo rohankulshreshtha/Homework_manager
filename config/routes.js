92f92edeb65f4caa9250c52b7065b41db37d1a82
rohankulshreshtha <rohankulshreshtha.bkn@gmail.com>
2016-12-29 12:46:50 +0530

    Add files via upload

---
+++ config/routes.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+//defining the routes
+
+module.exports = function(app) {
+	var hw = require('../controller/hw');
+	app.use(bodyParser.json());
+	app.get('/gethws', hw.gethws);
+	app.get('/getdonehws', hw.getdonehws);
+	app.get('/gethw/:subject', hw.gethw);
+	app.post('/addhw',hw.addhw);
+	app.get('/deletehw/:subject',hw.deletehw);
+	app.get('/donehw/:subject',hw.donehw);
+	app.get('/',hw.home);
+}
+
